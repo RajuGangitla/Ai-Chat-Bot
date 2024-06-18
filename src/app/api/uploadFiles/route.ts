90b0e6a611e22a15ff678facd073c99e23de3113
RajuGangitla <gangitlaraju8520@gmail.com>
2024-06-18 23:14:48 +0530

    filevectoisation done

--- src/app/api/uploadFiles/route.ts
+++ src/app/api/uploadFiles/route.ts
@@ -2,7 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import Files from "@/models/files"
 import mongoose from "mongoose";
 import { TextLoader } from "langchain/document_loaders/fs/text";
-
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { DocxLoader } from "@langchain/community/document_loaders/fs/docx";
+import { Pinecone } from "@pinecone-database/pinecone"
 
 export const POST = async (req: NextRequest) => {
 
@@ -18,6 +22,17 @@ export const POST = async (req: NextRequest) => {
             name: file?.name,
             userId: new mongoose.Types.ObjectId(userId as string),
         })
+
+
+        let metadata = {
+            userId: userId,
+            fileId: addFile?._id,
+            fileName: addFile.name
+        }
+
+        const isVectorised = await handleVectorisation(metadata, file)
+        addFile.isVectorised = isVectorised
+        addFile.save()
 
         return NextResponse.json(
             { success: true, addFile },
@@ -37,8 +52,66 @@ export const POST = async (req: NextRequest) => {
     }
 };
 
-export function handleVectorisation(name: string) {
-    if (name.split(".").pop() === "txt") {
+export async function handleVectorisation(metadata: Record<string, any>, file: any) {
+    try {
+        const pinecone = new Pinecone({
+            apiKey: process.env.PINECONE_API_KEY as string,
+        })
 
+        const pineconeIndex = pinecone.Index("files")
+        const embeddings = new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_TOKEN })
+        const blob = new Blob([file.buffer], { type: "application/octet-stream" })
+        let loader: any
+        if (file.name.split(".").pop() === "txt") {
+            loader = new TextLoader(blob)
+        }
+        if (file.name.split(".").pop() === "pdf") {
+            loader = new PDFLoader(blob)
+        }
+        if (file.name.split(".").pop() === "docx") {
+            loader = new DocxLoader(blob)
+        }
+        if (file.name.split(".").pop() === "doc") {
+            loader = new DocxLoader(blob)
+        }
+        // if (file.originalname.split(".").pop() === "csv") {
+        //     loader = new CSVLoader(blob)
+        // }
+
+        const docs: any = await loader.load()
+
+        for (let i = 0; i < docs.length; i++) {
+            docs[i].metadata = {
+                ...docs[i].metadata,
+                ...metadata,
+            }
+        }
+
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 500,
+            chunkOverlap: 100,
+        })
+
+        const output = await splitter.splitDocuments(docs)
+
+        let datatobeAdded: any = []
+        const embeddingPromises = await Promise.all(output.map(async (chunk, index) => {
+            const res = await embeddings.embedQuery(chunk?.pageContent)
+            let data = {
+                id: `${metadata?.fileName}`,
+                metadata: { ...metadata, text: chunk?.pageContent },
+                values: res,
+            }
+            datatobeAdded.push(data)
+        }
+        ));
+
+        // Upsert all chunks in one operation.
+        // await pinecone.index(name).namespace(`${metadata?.fileName}-${metadata?.filePath}`).upsert(datatobeAdded);
+        await pinecone.index("files").upsert(datatobeAdded);
+        return true
+    } catch (error) {
+        console.log(error, "errro message")
+        return false
     }
 }
